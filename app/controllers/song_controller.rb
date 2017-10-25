53247169a57d8884cf5943ade7e7bfcf75c09c5a
Mary Benitez <meebenitez@gmail.com>
2017-10-25 04:03:07 +0000

    automatically backed up by learn

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -42,7 +42,7 @@ class SongsController < ApplicationController
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
     @genres = Genre.all
-    erb :'songs/show'
+    erb :'songs/edit'
   end
 
 
